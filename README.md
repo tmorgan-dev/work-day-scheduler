fd89666f6998fdfcc007895fab8784f2642e8e7d
[Tim Morgan] <[tmorgan.dev.2023@gmail.com]>
2023-07-20 18:22:51 -0600

    cleaned up code added a readme

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # work-day-scheduler
+
+A tool designed to allow users to quickly plan out their work day
+
+## Description
+
+CLick into a time block to enter an objective for that hour. The current day is displayed at the top of the page. The current hour will be highlighted in red. Hours in the past are grey, time blocks in the future are green. Refresh the page to update the blocks before use. Upon refresh, any data input into the blocks will not be lost.
+
+## Installation
+
+Navigate to the following URL to access the page. It should display as the image below.
+
+## Usage
+
+Use this application to quickly and easily plan out your day, and see what day it is. 
+
+## Credits
+
+Xandromus wrote the HTML, CSS. Tim Morgan wrote the Javascript for this application
+
+## License
+
+Please refer to the license in the repo.
+
+## Screenshots
+
+![Screenshot 2023-07-20 180724](https://github.com/tmorgan-dev/work-day-scheduler/assets/132379127/77763ed7-5824-4300-be68-accda5c1f97b)
+
+
+
+## Link to deployed app
+
+https://tmorgan-dev.github.io/work-day-scheduler/
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,21 +1,4 @@
-// GIVEN I am using a daily planner to create a schedule
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
-// WHEN I scroll down
-// THEN I am presented with time blocks for standard business hours of 9am to 5pm
-// WHEN I view the time blocks for that day
-// THEN each time block is color-coded to indicate whether it is in the past, present, or future
-// WHEN I click into a time block
-// THEN I can enter an event
-// WHEN I click the save button for that time block
-// THEN the text for that event is saved in local storage
-// WHEN I refresh the page
-// THEN the saved events persist
-
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
+//function to allow users to save text in local storage and keep it upon a refresh
 $(function () {
   $('.saveBtn').on('click', function (){
     var description = $(this).siblings(".description").val()
@@ -25,24 +8,6 @@ $(function () {
   for (var i = 9; i <= 17; i++) {
     $(`#hour-${i} .description`).val(localStorage.getItem(`hour-${i}`))
   }
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
 });
 
 //var for the current day
